25e752be01b29ab8d4d2363b89f0a10647bf9778
turgut <turgutkafali@gmail.com>
2022-01-26 23:51:29 +0300

    form uygulama çalışması

--- çalışma3 js form ve submit/add.js
+++ çalışma3 js form ve submit/add.js
@@ -1,7 +1,24 @@
-let formDom = document.querySelector('#userForm');
-formDom.addEventListener('submit' , formSubmit);
+// let formDom = document.querySelector('#userForm');
+// formDom.addEventListener('submit' , formSubmit);
 
-function formSubmit(event){
-    event.preventDefault()
-    console.log('hersey yolunda');
-}
+// function formSubmit(event){
+//     event.preventDefault()
+//     console.log('hersey yolunda');
+// }
+
+
+
+var username;
+var password;
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("username:" + username + "-" + "password:" + password);
+  }
+  function handleChange(e) {
+    if (e.target.name === "username") {
+      username = e.target.value;
+    }
+    if (e.target.name === "password") {
+      password = e.target.value;
+    }
+  }
