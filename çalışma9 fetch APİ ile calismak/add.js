d706af66ed074cc4981f2f42a8813d3833c6ab00
turgut <turgutkafali@gmail.com>
2022-01-29 18:11:40 +0300

    fetch API işlemleri

---
+++ çalışma9 fetch APİ ile calismak/add.js
@@ -0,0 +1,29 @@
+
+
+// JSON dosyadan veri cektik
+
+// fetch('data/settings.json').then(
+//     response =>{
+//         return response.json()
+//         }
+//     ).then(responseJson =>{
+//         console.log(responseJson);
+//         console.log(responseJson.userName);
+//     });
+
+
+// API üzerinden veri çektik
+
+let userListDOM = document.querySelector('#userList');
+
+    fetch("https://jsonplaceholder.typicode.com/posts").then(
+        response => response.json()
+    ).then(
+        responseJson => {
+            responseJson.forEach(item =>{
+                let liDOM = document.createElement('li');
+                liDOM.innerHTML = item.title;
+                userListDOM.append(liDOM);
+            })
+        }
+    )
